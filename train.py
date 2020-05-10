b170ca74e3346ad530c9483a4d3cc82ac246d2f0
Len HIRATA <len@hm00m26.local>
2020-05-10 13:00:05 +0900

    adfa

--- train.py
+++ train.py
@@ -7,8 +7,13 @@ import numpy as np
 
 from fizzbuzz import FizzBuzz
 
-class QNetwork:
-    def __init__(self, learning_rate=0.01, state_size=15, action_size=4, hidden_size=16):
+class Model:
+    def __init__(self):
+        learning_rate = 0.01
+        state_size    = 15
+        action_size   = 4
+        hidden_size   = 16
+
         self.model = Sequential()
         self.model.add(Dense(hidden_size, activation='relu', input_dim=state_size))
         self.model.add(Dense(action_size, activation='softmax'))
@@ -16,67 +21,67 @@ class QNetwork:
         self.model.compile(loss='mse', optimizer=self.optimizer)
         self.model.summary()
 
-    def replay(self, memory, batch_size, gamma, targetQN):
+    def replay(self, memory, batch_size, gamma, target_model):
         inputs     = np.zeros((batch_size, 15))
-        targets    = np.zeros((batch_size, 4))
+        outputs    = np.zeros((batch_size, 4))
         mini_batch = memory.sample(batch_size)
 
-        for i, (state_b, action_b, reward_b, next_state_b) in enumerate(mini_batch):
-            inputs[i:i + 1] = state_b
-            target = reward_b
+        for i, (state, action, reward, next_state) in enumerate(mini_batch):
+            inputs[i:i + 1] = state
+            target          = reward
 
-            if not (next_state_b == np.zeros(state_b.shape)).all():
-                retmainQs = self.model.predict(next_state_b.reshape(1, 15))[0].argmax()
+            if not (next_state == np.zeros(state.shape)).all():
+                retmainQs = self.model.predict(next_state.reshape(1, 15))[0].argmax()
                 next_action = np.argmax(retmainQs)
-                target = reward_b + gamma * targetQN.model.predict(next_state_b.reshape(1, 15))[0][next_action]
+                target = reward + gamma * target_model.model.predict(next_state.reshape(1, 15))[0][next_action]
 
-            targets[i] = self.model.predict(state_b.reshape(1, 15))
-            targets[i][action_b.argmax()] = target
+            outputs[i] = self.model.predict(state.reshape(1, 15))
+            outputs[i][action.argmax()] = target
 
-        self.model.fit(inputs, targets, epochs=1, verbose=0)
+        self.model.fit(inputs, outputs, epochs=1, verbose=0)
 
 class Memory:
-    def __init__(self, max_size=1000):
-        self.buffer = deque(maxlen=max_size)
+    def __init__(self):
+        self.buffer = deque()
 
-    def add(self, experience):
-        self.buffer.append(experience)
+    def add(self, exp):
+        self.buffer.append(exp)
 
     def sample(self, batch_size):
-        idx = np.random.choice(np.arange(len(self.buffer)), size=batch_size, replace=False)
-        return [self.buffer[ii] for ii in idx]
-
-    def len(self):
-        return len(self.buffer)
+        indice = np.random.choice(np.arange(len(self.buffer)), size=batch_size, replace=False)
+        return [self.buffer[i] for i in indice]
 
 class Agent:
-    def get_action(self, state, epoch, main_qn):
+    def get_action(self, state, epoch, main_model):
         epsilon = 0.001 + 0.9 / (1.0 + epoch)
 
-        if epsilon <= np.random.uniform(0, 1):
-            retTargetQs = main_qn.model.predict(state.reshape(1, 15))[0]
-            action = np.argmax(retTargetQs)
+        if epsilon < np.random.uniform(0, 1):
+            action = main_model.model.predict(state.reshape(1, 15))[0].argmax()
         else:
             action = np.random.choice([0, 1, 2, 3])
 
         return to_categorical(action, 4)
 
-    def save(self):
-        return
+def evaluate(env):
+    env.reset()
+    state, _, finished = env.random_step()
+    while not finished:
+        action = agent.get_action(state, N_EPOCHS, main_model)
+        next_state, _, finished = env.step(action.argmax(), verbose=True)
+        state = next_state
 
 if __name__ == '__main__':
-    N_EPOCHS = 10000
+    N_EPOCHS = 5000
     S_BATCH  = 4
     GAMMA    = 0.99
 
-    env = FizzBuzz(1, 100)
+    env = FizzBuzz(1, 1000)
 
-    main_qn   = QNetwork(learning_rate=0.0001)
-    target_qn = QNetwork(learning_rate=0.0001)
+    main_model   = Model()
+    target_model = Model()
 
-    memory = Memory(max_size=10000)
-
-    agent = Agent()
+    memory = Memory()
+    agent  = Agent()
 
     learned_flag = False
 
@@ -87,29 +92,23 @@ if __name__ == '__main__':
         print('Epoch: {}'.format(epoch + 1))
 
         env.reset()
-        state, reward, terminal = env.random_step()
+        state, reward, finished = env.random_step()
 
-        target_qn.model.set_weights(main_qn.model.get_weights())
+        target_model.model.set_weights(main_model.model.get_weights())
 
-        while not terminal:
-            action = agent.get_action(state, epoch, main_qn)
+        while not finished:
+            action = agent.get_action(state, epoch, main_model)
             learned_flag = env.is_learned()
-            next_state, reward, terminal = env.step(action.argmax())
+            next_state, reward, finished = env.step(action.argmax())
 
             memory.add((state, action, reward, next_state))
 
             state = next_state
 
-            if memory.len() > S_BATCH:
-                main_qn.replay(memory, S_BATCH, GAMMA, target_qn)
+            if len(memory.buffer) > S_BATCH:
+                main_model.replay(memory, S_BATCH, GAMMA, target_model)
 
-            target_qn.model.set_weights(main_qn.model.get_weights())
+            target_model.model.set_weights(main_model.model.get_weights())
 
-    env.reset()
-    state, _, terminal = env.random_step()
-    while not terminal:
-        action = agent.get_action(state, 10000, main_qn)
-        next_state, _, terminal = env.step(action.argmax(), verbose=True)
-        state = next_state
-
-    agent.save()
+    env = FizzBuzz(500, 1000)
+    evaluate(env)
